75ff496ba14708a261e059e9188c43f9585cc6d2
Felipe Bona <felipechiodinibona@hotmail.com>
2024-04-07 15:09:06 -0300

    melhorando tela de login

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="container" style="height: 100vh;">
-    <div class="d-flex flex-column justify-content-center align-items-center h-100">
-      <div class="bg-white rounded p-5 w-100 shadow" style="max-width: 500px;">
-        <div class="text-center text-muted mb-4">
-          <h4>Burguer System</h4>
+  <div style="height: 100vh;">
+    <div class="d-flex h-100">
+      <img src="/owner.png" class="shadow-lg rounded-end-4" alt="owner rapideats sistem" style="height: 100%; width: 50%; object-fit: cover;">
+      <div class="d-flex flex-column justify-content-center align-items-center h-100 w-50">
+        <div class="w-75 ">
+          <h5>Rapid Eats</h5>
+          <h3>Entrar</h3>
+          <p>Acesse sua conta para gerenciar sua loja.</p>
+          <form class="d-flex flex-column" @submit.prevent="onSubmit()">
+            <input required class="form-control" placeholder="Email" type="email" v-model="form.email" />
+            <input required class="form-control my-2" placeholder="Senha" type="password" v-model="form.password" />
+            <RouterLink class="mt-3" :to="{ name: 'auth.forget-password' }">
+              Esqueci minha senha.
+            </RouterLink>
+            <BaseButton style="padding: 1rem; border-radius: 50px; width: 250px; margin-top: 1rem;" type="submit" :loading="submiting">
+              Entrar
+            </BaseButton>
+            <div v-bind="errors.has('error')">
+              {{ errors.get('error') }}
+            </div>
+          </form>
         </div>
-        <form @submit.prevent="onSubmit()">
-          <input class="form-control" placeholder="Email" type="email" v-model="form.email" />
-          <input class="form-control my-2" placeholder="Senha" type="password" v-model="form.password" />
-          <base-button class="w-100" type="submit" :loading="submiting">
-            Entrar
-          </base-button>
-          <div v-bind="errors.has('error')">
-            {{ errors.get('error') }}
-          </div>
-        </form>
       </div>
-      <router-link class="mt-3" :to="{ name: 'auth.forget-password' }">Esqueci minha senha.</router-link>
     </div>
   </div>
 </template>
