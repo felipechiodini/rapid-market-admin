49bbb0e327876005d768bbab971922d34c139f7b
Felipe Bona <felipechiodinibona@hotmail.com>
2024-04-07 14:14:53 -0300

    ajusts

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 // import { useUserStore } from '@/stores/user'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
   ],
   resolve: {
-    alias: 
-      [
-        { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      ]
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   server: {
     port: 8081
