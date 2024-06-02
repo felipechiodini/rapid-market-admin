59c39199d15a9e74bed58e90613180321c9aa6cb
Felipe Bona <felipechiodinibona@hotmail.com>
2024-06-02 10:13:41 -0300

    remove button toogle sidebar

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="wraper" :class="{ 'collapsed': collapsed }">
     <template v-if="loading === false && store">
-      <button class="btn" @click="toggleCollapsed()">
+      <!-- <button class="btn" @click="toggleCollapsed()">
         <span :class="{ 'fas fa-chevron-left float-end': collapsed === false, 'fas fa-chevron-right': collapsed === true }"></span>
-      </button>
+      </button> -->
       <StoreCard v-if="collapsed === false" />
       <ul class="m-0">
         <li v-for="(menu, key) in sidebar" :key="key">
